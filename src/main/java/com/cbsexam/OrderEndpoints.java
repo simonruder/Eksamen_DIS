745e63c5c79736e5ae338acebfc0950d3cab2c5e
simonruder <siru17ab@student.cbs.dk>
2018-10-19 17:18:03 +0200

    Jeg har tilføjet kryptering i Order-klassen

--- src/main/java/com/cbsexam/OrderEndpoints.java
+++ src/main/java/com/cbsexam/OrderEndpoints.java
@@ -11,6 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import model.Order;
+import utils.Encryption;
 
 @Path("order")
 public class OrderEndpoints {
@@ -26,9 +27,13 @@ public class OrderEndpoints {
     // Call our controller-layer in order to get the order from the DB
     Order order = OrderController.getOrder(idOrder);
 
-    // TODO: Add Encryption to JSON
+    // TODO: Add Encryption to JSON : FIX
     // We convert the java object to json with GSON library imported in Maven
     String json = new Gson().toJson(order);
+
+    //SIMON - Tilføjer kryptering til getOrder
+    json = Encryption.encryptDecryptXOR(json);
+
 
     // Return a response with status 200 and JSON as type
     return Response.status(200).type(MediaType.APPLICATION_JSON).entity(json).build();
@@ -45,6 +50,9 @@ public class OrderEndpoints {
     // TODO: Add Encryption to JSON
     // We convert the java object to json with GSON library imported in Maven
     String json = new Gson().toJson(orders);
+
+    //SIMON - Kryptering er tilføjet
+    json=Encryption.encryptDecryptXOR(json);
 
     // Return a response with status 200 and JSON as type
     return Response.status(200).type(MediaType.TEXT_PLAIN_TYPE).entity(json).build();
